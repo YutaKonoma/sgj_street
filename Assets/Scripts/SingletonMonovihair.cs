635439cdd0df6dd6187e7d48d6c11058c76a2181
DRGO <abokado0901@gmail.com>
2023-03-26 12:33:44 +0900

    Score,Time,GameOver処理の追加

---
+++ Assets/Scripts/SingletonMonovihair.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class SingletonMonovihair<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected abstract bool _dontDestroyOnLoad { get; }
+
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                Type t = typeof(T);
+                instance = (T)FindObjectOfType(t);
+                if (!instance)
+                {
+                    Debug.LogError(t + "はありません。");
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+        if (_dontDestroyOnLoad)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
