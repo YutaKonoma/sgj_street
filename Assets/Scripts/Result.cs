05fbb3ef03fd5fe2d6bf322f4a92d33c1116412c
miyn <20miyu03@gmail.com>
2023-03-27 13:10:48 +0900

    リザルトスクリプト変更

--- Assets/Scripts/Result.cs
+++ Assets/Scripts/Result.cs
@@ -5,10 +5,8 @@ using UnityEngine.UI;
 
 public class Result : MonoBehaviour
 {
-    [Tooltip("～以下の演出")]
-    [SerializeField]float _lowScore;
-    [Tooltip("～以上の演出")]
-    [SerializeField]float _hightScore;
+    [Tooltip("1.一番いい　2.普通　3.低い　4.一番ダメ　リザルトのスコア")]
+    [SerializeField]float[] _resultLevel = new float[4];
 
     /// <summary>スコア</summary>
     [SerializeField]float _score;
@@ -18,8 +16,8 @@ public class Result : MonoBehaviour
     [SerializeField]Image _background;
     [Tooltip("スコアテキスト")]
     [SerializeField]Text _scoreText;
-    [Tooltip("1.一番いい背景　2.普通の背景　3.ダメな背景")]
-    [SerializeField]Sprite[] _resultBackground;
+    [Tooltip("1.一番いい背景　2.普通の背景　3.低い背景　3.ダメな背景")]
+    [SerializeField]Sprite[] _resultBackground = new Sprite[4];
     
 
     void Start()
@@ -30,19 +28,24 @@ public class Result : MonoBehaviour
         //スコアテキストを更新
         _scoreText.text = $"{_score}";
 
-        if(_score >= _hightScore)
+        if(_score >= _resultLevel[0])
         {
             _background.sprite = _resultBackground[0];
         }//一番いいリザルト
 
-        else if(_score >= _lowScore)
+        else if(_score >= _resultLevel[1])
         {
             _background.sprite = _resultBackground[1];
         }//普通のリザルト
 
-        else
+        else if(_score >= _resultLevel[2])
         {
             _background.sprite = _resultBackground[2];
+        }//低いときのリザルト
+
+        else if(_score >= _resultLevel[3])
+        {
+            _background.sprite = _resultBackground[3];
         }//ダメだった時のリザルト
     }
 }
